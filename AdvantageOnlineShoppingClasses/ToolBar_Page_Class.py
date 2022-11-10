538bb110479ffcc04e0fbf5b329e1e6d94b7fdf8
admin <omeruzan14@gmail.com>
2022-11-10 14:48:45 +0200

    TEST START V1.18

--- AdvantageOnlineShoppingClasses/ToolBar_Page_Class.py
+++ AdvantageOnlineShoppingClasses/ToolBar_Page_Class.py
@@ -170,7 +170,7 @@ class ToolBarClass:
 
     def Wait_UserSignOut(self):
         "Waits for the user to be signed in"
-        self.wait.until(EC.text((By.CSS_SELECTOR, "#menuUserLink>span"), ""))
+        self.wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, "#menuUserLink>span"), ""))
 
     def Get_UserIconName_Element(self):
        return self.driver.find_element(By.CSS_SELECTOR,"#menuUserLink>span")
